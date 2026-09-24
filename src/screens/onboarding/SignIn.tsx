c8ac9c8064448ccb4c00e52ae10afc36851af3cd
Abhishek Reddy <127029931+Abhishhhhh@users.noreply.github.com>
2026-09-24 16:19:22 +0530

    fix: drop duplicate Spork title on sign-in (10/12)

--- src/screens/onboarding/SignIn.tsx
+++ src/screens/onboarding/SignIn.tsx
@@ -53,7 +53,7 @@ export default function SignIn() {
 
   return (
     <div className="screen min-h-screen animate-fade-in">
-      <TopBar title="Spork" back="/welcome" />
+      <TopBar title="" back="/welcome" />
 
       <div className="icon-box mx-auto" style={{ width: 120, height: 120, marginTop: 24 }}><SporkWordmark size={28} play="once" /></div>
       <div style={{ height: 28 }} />
